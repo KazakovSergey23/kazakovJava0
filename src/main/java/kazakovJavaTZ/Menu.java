8b4e9dff695c0043d503cb24058f59ccc59454b3
Sergey <leemmbo@mail.ru>
2024-01-20 14:59:58 +0300

    баги 1.2 добавил try catch

--- src/main/java/kazakovJavaTZ/Menu.java
+++ src/main/java/kazakovJavaTZ/Menu.java
@@ -1,5 +1,6 @@
 package kazakovJavaTZ;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -18,13 +19,18 @@ public class Menu {
         System.out.println("Задание 1. \nВывести (привет) если введут цифру больше 7 ");
         System.out.println();
         System.out.print("Введите: ");
-        int number = scanner.nextInt();
-        if (number > 7 && number < 2147483647) {
-            System.out.println("Привет");
-        } else {
-            System.out.println("Введите больше 7");
+        try {
+            int number = scanner.nextInt();
+            if (number > 7 && number < 2147483647) {
+                System.out.println("Привет");
+            } else {
+                System.out.println("Введите больше 7");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка: введено не число");
+            scanner.nextLine(); // Очистка буфера ввода
+            exeOne();
         }
-
     }
 
     public void exeTwo() {
@@ -32,12 +38,19 @@ public class Menu {
         System.out.println("Задание 2. \nВывести Привет,Вячеслав если совпадет ввод с именем Вячеслав");
         System.out.println();
         System.out.print("Введите строку: ");
-        String str = scanner.nextLine();
-        if ("Вячеслав".equals(str)) {
-            System.out.println("-----------------");
-            System.out.println("Привет, Вячеслав");
-        } else {
-            System.out.println("Нет такого имени");
+        try {
+            String str = scanner.nextLine();
+            if ("Вячеслав".equals(str)) {
+                System.out.println("-----------------");
+                System.out.println("Привет, Вячеслав");
+            } else {
+                System.out.println("Нет такого имени");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка: введено не число");
+            scanner.nextLine(); // Очистка буфера ввода
+            exeTwo();
+
         }
     }
 
@@ -48,26 +61,35 @@ public class Menu {
         System.out.println();
         System.out.println("Для начала создадим массив.");
         System.out.print("Введите размер массива(max = 30): ");
-        int lenght = scanner.nextInt();
-        if (lenght > 0 && lenght <= 30) {
-            mass = new int[lenght];
-            System.out.println("Условия соблюдены, теперь заполним массив цифрами");
-            for (int i = 0; i < lenght; i++) {
-                int number = scanner.nextInt();
-                if (number > 0 && number <= 100) {
-                    mass[i] = number;
-                } else {
-                    System.out.println("Введите снова число так как это число находится " +
-                            "не в заданном диапазоне: от 1 до 100.");
-                    i--;
+        try {
+            int lenght = scanner.nextInt();
+            if (lenght > 0 && lenght <= 30) {
+                mass = new int[lenght];
+                System.out.println("Условия соблюдены, теперь заполним массив цифрами");
+                for (int i = 0; i < lenght; i++) {
+                    int number = scanner.nextInt();
+                    if (number > 0 && number <= 100) {
+                        mass[i] = number;
+                    } else {
+                        System.out.println("Введите снова число, так как это число находится " +
+                                "не в заданном диапазоне: от 1 до 100.");
+                        i--;
+                    }
                 }
-            }
-            for (int i = 0; i < mass.length; i++) {
-                if (mass[i] != 0 && mass[i] % 3 == 0) {
-                    System.out.println("Номер элемента массива: " + i
-                            + ". значение кратное трём: " + mass[i]);
+                for (int i = 0; i < mass.length; i++) {
+                    if (mass[i] != 0 && mass[i] % 3 == 0) {
+                        System.out.println("Номер элемента массива: " + i
+                                + ". значение кратное трём: " + mass[i]);
+                    }
                 }
+            } else {
+                System.out.println("Введите число в диапазоне от 1 до 30");
+                exeThree();
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели не число.");
+            exeThree();
+            scanner.nextLine();
         }
     }
 
